9684ead9baf2e551110c06ab14f60d42e78411ff
semyon-dev <semyon-dev@protonmail.com>
2020-05-07 23:05:01 +0300

    feat: connection to geth

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/whisper/shhclient"
+)
+
 func main() {
-	
+	client, err := shhclient.Dial("ws://127.0.0.1:8546")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = client // we'll be using this in the next section
+	fmt.Println("we have a whisper connection")
 }
